a4504e2c385a141a5fa09556b6e53e0f1f4b8863
Yeison Villegas <yeisonvillegas07@gmail.com>
2025-01-30 19:23:36 -0500

    .env configured successfully

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,24 +9,32 @@ import { Category } from './category/entities/category.entity';
 import { CategoryModule } from './category/category.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ThrottlerModule.forRoot([
       {
         ttl: 10000,
         limit: 100,
       },
     ]),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'supersecret',
-      database: 'taskTracker',
-      entities: [User, Expense, Category],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASS'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [User, Expense, Category],
+        synchronize: true,
+      }),
     }),
     UserModule,
     AuthModule,
